bdf4d8a2296f6305662983af5f480ce7e4a69a46
Zaxaar <zahar1982@gmail.com>
2016-06-20 18:43:26 +0200

    Dodanie zajawki i usunięcie jumbotrona.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// JavaScript Document
+// “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”
+
 $(document).ready(function() {
 	
 	// Navigation Toggle starts here 
@@ -12,11 +13,27 @@ $(document).ready(function() {
   });
   // Navigation Toggle ends here
 
-  // Jumbotron background change
-  var bgi = ["1.jpg", "2.jpg", "3.jpg"];
-  var bgiRandom = bgi[Math.floor(Math.random()*bgi.length)];
+  // Main Teaser Image Random Switch/Animation starts here
+  var bgi = ["1.jpg", "2.jpg", "3.jpg"]; // images list
+  var bgiRandom = bgi[Math.floor(Math.random()*bgi.length)]; // random image select
 
-  $(".jumbotron").fadeIn("slow").css('background', 'url("images/main_image/'+ bgiRandom +'")no-repeat center');
+  $(".teaser-image").css('background-image', 'url("images/main_image/' + bgiRandom + '")').animate({"opacity": "1"}, 150 );
+  // Main Teaser Image Random Switch/Animation ends here
+
+  // MainMenu page scrolling starts here
+  // $('a[href*="#"]:not([href="#"])').click(function() {
+  //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //     if (target.length) {
+  //       $('html, body').animate({
+  //         scrollTop: target.offset().top
+  //       }, 1000);
+  //       return false;
+  //     }
+  //   }
+  // });
+  // MainMenu page scrolling ends here
 
 });
 
