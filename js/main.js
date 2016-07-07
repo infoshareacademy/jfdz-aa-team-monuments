1b92f3c0f85ec002428a61f324d2f6a06d3fa566
Zaxaar <zahar1982@gmail.com>
2016-07-07 10:06:50 +0200

    Zmieniające się interwałowo zdjęcie w zajawce.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 // “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”
 
 $(document).ready(function () {
-
-
-
-
+    
     // Navigation Toggle starts here
     $(".navbar-toggle").click(function () {
         $("#mainNavbar").toggle();
@@ -16,11 +13,14 @@ $(document).ready(function () {
     });
     // Navigation Toggle ends here
 
-    // Main Teaser Image Random Switch/Animation starts here
-    var bgi = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]; // images list
-    var bgiRandom = bgi[Math.floor(Math.random() * bgi.length)]; // random image select
+    // Main Teaser Image Switch/Animation starts here
+    var bgi = ["2.jpg", "3.jpg", "4.jpg","1.jpg"]; // images list
 
-    $(".teaser-image").css('background-image', 'url("images/main_image/' + bgiRandom + '")').animate({"opacity": "1"}, 150);
+    setInterval(function(){
+    var bgiNext = bgi.shift();
+        bgi.push(bgiNext);
+        $(".teaser-image").css("opacity", "0").css('background-image', 'url("images/main_image/' + bgiNext + '")').animate({opacity: "1"});
+     }, 5000);
     // Main Teaser Image Random Switch/Animation ends here
 
     // MainMenu page scrolling starts here
@@ -42,9 +42,8 @@ $(document).ready(function () {
     var cookieList = document.cookie.split(";");
     for (var i = 0; i < cookieList.length; i++) {
         var cookieElement = cookieList[i];
-        if (cookieElement == "cookies-note=confirmed") {//trzeba zmienić wartość ciasteczka na confirmed
+        if (cookieElement == "cookies-note=confirmed") {
             $("#cookies-alert").hide();
-
         }
     }
 
@@ -52,12 +51,11 @@ $(document).ready(function () {
         document.cookie = "cookies-note=confirmed";
         $("#cookies-alert").hide();
     });
-    
     // Cookies alert control ends here
 
     //Contact form fields disable //
     $(".antibot").click(function () {
         $(".locked").prop("disabled", !$(this).is(":checked"));
     })
-});
-
+    
+}); // Document Last Line  
