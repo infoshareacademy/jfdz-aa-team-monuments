cb538b8048e0f85adfa7bddd49f0d9d44abdba35
michal_z <zahar1982@gmail.com>
2016-07-17 17:04:14 +0200

    Merge branch 'master' of https://github.com/infoshareacademy/jfdz-aa-team-monuments
    Conflicts:
    	index.html
    Poprawiony mechanizm mainMenu

--- js/main.js
+++ js/main.js
@@ -24,17 +24,18 @@ $(document).ready(function () {
     // Main Teaser Image Switch/Animation ends here
 
     // MainMenu page scrolling starts here
-    $('body').scrollspy({target: ".navbar"});
-    $('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top,
-                }, 1000);
-                return false;
-            }
+    $('body').scrollspy({target: ".navbar", offset: 50});
+    $(".navbar a, .back a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
         }
     });
     // MainMenu page scrolling ends here
